282dcf08b20f88e558ca96ce5bfe561678987d31
MUHAMMAD FAKHRUR RIZAL <fakhrurrizal10@gmail.com>
2024-02-11 14:13:11 +0700

    implementasi CRUD User Product, dan category

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        \App\Models\User::factory(20)->create();
+
+        \App\Models\User::factory()->create([
+            'name' => 'Test Admin',
+            'email' => 'test@admin.com',
+            'password' => Hash::make('Password@123'),
+            'role' => 'admin'
+        ]);
+
+        $this->call([
+            CategorySeeder::class,
+            ProductSeeder::class,
+        ]);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CategoryController;
+
+
+Route::get('/', function () {
+    return view('pages.auth.login');
+});
+
+
+
+Route::middleware(['auth'])->group(function (){
+    Route::get('home', function () {
+        return view('pages.dashboard');
+    })->name('home');
+
+    Route::resource('users', UserController::class);
+    Route::resource('products', ProductController::class);
+    Route::resource('categories', CategoryController::class);
+});
